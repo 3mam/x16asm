3749ddc051215eb727994e895e6e839f4892cc36
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2021-02-14 01:59:15 +0100

    reduce cases in switch

--- asm.js
+++ asm.js
@@ -45,16 +45,6 @@ const parseAsm = asmSource => (command = [], cursorPosition = 0, startPoint = 0,
 		case "$":
 		case "#":
 		case "%":
-		case "0":
-		case "1":
-		case "2":
-		case "3":
-		case "4":
-		case "5":
-		case "6":
-		case "7":
-		case "8":
-		case "9":
 			if (type === 0)
 				return [command, cursorPosition + 1, cursorPosition, VALUE]
 			break
