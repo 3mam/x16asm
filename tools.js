50a63194fc1f00e4902c1eee6f183c759fcd5814
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2021-03-12 05:21:10 +0100

    update tools

--- tools.js
+++ tools.js
@@ -1,4 +1,22 @@
-export default func => (...variables) => {
+function objProtect(obj, fn) {
+	if (Array.isArray(obj))
+		return [...fn(obj)]
+	else if (typeof obj === 'object')
+		return { ...obj, ...fn(obj) }
+	else
+		return fn(obj)
+}
+
+function pipe(obj) {
+	Object.freeze(obj)
+	this.valueOf = () => obj
+	this.pipe = (fn) => new pipe(objProtect(obj, fn))
+}
+
+export const piping = obj => new pipe(obj)
+export const composition = (...fn) => val => fn.reduce((v, f) => f(v), val)
+
+export const recursion = func => (...variables) => {
 	try {
 		while (true)
 			variables = func(...variables)
