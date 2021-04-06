8794d95f1739a1591d99e4a876e04b3f4cd61bc5
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2021-04-07 00:39:33 +0200

    parsing more instructions

--- parser.js
+++ parser.js
@@ -18,41 +18,122 @@ const error = ({ line, column }, errorMessage) => {
 const checkUnknown = ({ type, line, column }) =>
 	type === types.UNKNOWN && error({ line, column }, 'Unknown instruction')
 
-const isLabelIndirect = (tokens, index) =>
-	tokens[index].type === types.OPEN_BRACKET &&
-	tokens[index + 1].type === types.LABEL_VALUE &&
-	tokens[index + 2].type === types.CLOSE_BRACKET &&
-	tokens[index + 3].type !== types.COMMA
+const instructionsIdentification = (tokens, index) => ({
+	get isLabelIndirect() {
+		return (
+			tokens[index]?.type === types.OPEN_BRACKET &&
+			tokens[index + 1]?.type === types.LABEL_VALUE &&
+			tokens[index + 2]?.type === types.CLOSE_BRACKET &&
+			tokens[index + 3]?.type !== types.COMMA
+		)
+	},
+	get isLabelIndirectX() {
+		return (
+			tokens[index]?.type === types.OPEN_BRACKET &&
+			tokens[index + 1]?.type === types.LABEL_VALUE &&
+			tokens[index + 2]?.type === types.COMMA &&
+			tokens[index + 3]?.type === types.X &&
+			tokens[index + 4]?.type === types.CLOSE_BRACKET
+		)
+	},
+	get isLabelIndirectY() {
+		return (
+			tokens[index]?.type === types.OPEN_BRACKET &&
+			tokens[index + 1]?.type === types.LABEL_VALUE &&
+			tokens[index + 2]?.type === types.CLOSE_BRACKET &&
+			tokens[index + 3]?.type === types.COMMA &&
+			tokens[index + 4]?.type === types.Y
+		)
+	},
+	get isIndirect() {
+		return (
+			tokens[index]?.type === types.OPEN_BRACKET &&
+			tokens[index + 1]?.type === types.HEX_VALUE &&
+			tokens[index + 2]?.type === types.NUMBER &&
+			tokens[index + 3]?.type === types.CLOSE_BRACKET &&
+			tokens[index + 4]?.type !== types.COMMA
+		)
+	},
+	get isIndirectX() {
+		return (
+			tokens[index]?.type === types.OPEN_BRACKET &&
+			tokens[index + 1]?.type === types.HEX_VALUE &&
+			tokens[index + 2]?.type === types.NUMBER &&
+			tokens[index + 3]?.type === types.COMMA &&
+			tokens[index + 4]?.type === types.X &&
+			tokens[index + 5]?.type === types.CLOSE_BRACKET
+		)
+	},
+	get isIndirectY() {
+		return (
+			tokens[index]?.type === types.OPEN_BRACKET &&
+			tokens[index + 1]?.type === types.HEX_VALUE &&
+			tokens[index + 2]?.type === types.NUMBER &&
+			tokens[index + 3]?.type === types.CLOSE_BRACKET &&
+			tokens[index + 4]?.type === types.COMMA &&
+			tokens[index + 5]?.type === types.Y
+		)
+	},
+	get isHexValue() {
+		return (
+			tokens[index]?.type === types.VALUE &&
+			tokens[index + 1]?.type === types.HEX_VALUE &&
+			tokens[index + 2]?.type === types.NUMBER
+		)
+	},
+	get isBinValue() {
+		return (
+			tokens[index]?.type === types.VALUE &&
+			tokens[index + 1]?.type === types.BIN_VALUE &&
+			tokens[index + 2]?.type === types.NUMBER
+		)
+	},
+	get isValue() {
+		return (
+			tokens[index]?.type === types.VALUE &&
+			tokens[index + 1]?.type === types.NUMBER
+		)
+	}
+})
 
-const isLabelIndirectX = (tokens, index) =>
-	tokens[index].type === types.OPEN_BRACKET &&
-	tokens[index + 1].type === types.LABEL_VALUE &&
-	tokens[index + 2].type === types.COMMA &&
-	tokens[index + 3].type === types.X &&
-	tokens[index + 4].type === types.CLOSE_BRACKET
+const split16to8bit = val => val > 255 ? [0xff & val, val >> 8 & 0xff] : val
 
-const isLabelIndirectY = (tokens, index) =>
-	tokens[index].type === types.OPEN_BRACKET &&
-	tokens[index + 1].type === types.LABEL_VALUE &&
-	tokens[index + 2].type === types.CLOSE_BRACKET &&
-	tokens[index + 3].type === types.COMMA &&
-	tokens[index + 4].type === types.Y
+const strHexToValue = valueStrHex => split16to8bit(parseInt(valueStrHex, 16))
+const strDcmToValue = valueStrDcm => split16to8bit(parseInt(valueStrDcm, 10))
+const strBinToValue = valueStrBin => split16to8bit(parseInt(valueStrBin, 2))
+
 
 const getAddressingTypeFromValueToken = (tokens, index = 0, newTokensList = []) => {
-	if (tokens.length - 1 === index)
+	if (tokens.length <= index)
 		throw newTokensList
-
-
-	if (isLabelIndirectY(tokens, index))
+	const instruction = instructionsIdentification(tokens, index)
+	if (instruction.isLabelIndirectY)
 		return [tokens, index + 5, [...newTokensList,
 		{ value: tokens[index + 1].instruction, type: addressingType.INDIRECT_LABEL_Y }]]
-	else if (isLabelIndirectX(tokens, index))
+	else if (instruction.isLabelIndirectX)
 		return [tokens, index + 5, [...newTokensList,
 		{ value: tokens[index + 1].instruction, type: addressingType.INDIRECT_LABEL_X }]]
-	else if (isLabelIndirect(tokens, index))
+	else if (instruction.isLabelIndirect)
 		return [tokens, index + 3, [...newTokensList,
 		{ value: tokens[index + 1].instruction, type: addressingType.INDIRECT_LABEL }]]
-
+	else if (instruction.isIndirectY)
+		return [tokens, index + 6, [...newTokensList,
+		{ value: strHexToValue(tokens[index + 2].instruction), type: addressingType.INDIRECT_Y }]]
+	else if (instruction.isIndirectX)
+		return [tokens, index + 6, [...newTokensList,
+		{ value: strHexToValue(tokens[index + 2].instruction), type: addressingType.INDIRECT_X }]]
+	else if (instruction.isIndirect)
+		return [tokens, index + 4, [...newTokensList,
+		{ value: strHexToValue(tokens[index + 2].instruction), type: addressingType.INDIRECT }]]
+	else if (instruction.isHexValue)
+		return [tokens, index + 3, [...newTokensList,
+		{ value: strHexToValue(tokens[index + 2].instruction), type: -1 }]]
+	else if (instruction.isBinValue)
+		return [tokens, index + 3, [...newTokensList,
+		{ value: strBinToValue(tokens[index + 2].instruction), type: -1 }]]
+	else if (instruction.isValue)
+		return [tokens, index + 2, [...newTokensList,
+		{ value: strDcmToValue(tokens[index + 1].instruction), type: -1 }]]
 
 	return [tokens, index + 1, [...newTokensList, tokens[index]]]
 }
