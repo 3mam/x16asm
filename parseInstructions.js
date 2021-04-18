75174a564c16f2ccc81188b1c6120391e628be2b
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2021-04-18 12:46:40 +0200

    format code

--- parseInstructions.js
+++ parseInstructions.js
@@ -2,126 +2,126 @@ import { types } from './enum.js'
 
 export const recognizeInstructionsValue = (tokens, index) => ({
 	isLabelIndirect: () =>
-		tokens[index]?.type === types.OPEN_BRACKET &&
-		tokens[index + 1]?.type === types.LABEL_VALUE &&
-		tokens[index + 2]?.type === types.CLOSE_BRACKET &&
-		tokens[index + 3]?.type !== types.COMMA
+		tokens[index]?.type === types.OPEN_BRACKET
+		&& tokens[index + 1]?.type === types.LABEL_VALUE
+		&& tokens[index + 2]?.type === types.CLOSE_BRACKET
+		&& tokens[index + 3]?.type !== types.COMMA
 	,
 	isLabelIndirectX: () =>
-		tokens[index]?.type === types.OPEN_BRACKET &&
-		tokens[index + 1]?.type === types.LABEL_VALUE &&
-		tokens[index + 2]?.type === types.COMMA &&
-		tokens[index + 3]?.type === types.X &&
-		tokens[index + 4]?.type === types.CLOSE_BRACKET
+		tokens[index]?.type === types.OPEN_BRACKET
+		&& tokens[index + 1]?.type === types.LABEL_VALUE
+		&& tokens[index + 2]?.type === types.COMMA
+		&& tokens[index + 3]?.type === types.X
+		&& tokens[index + 4]?.type === types.CLOSE_BRACKET
 	,
 	isLabelIndirectY: () =>
-		tokens[index]?.type === types.OPEN_BRACKET &&
-		tokens[index + 1]?.type === types.LABEL_VALUE &&
-		tokens[index + 2]?.type === types.CLOSE_BRACKET &&
-		tokens[index + 3]?.type === types.COMMA &&
-		tokens[index + 4]?.type === types.Y
+		tokens[index]?.type === types.OPEN_BRACKET
+		&& tokens[index + 1]?.type === types.LABEL_VALUE
+		&& tokens[index + 2]?.type === types.CLOSE_BRACKET
+		&& tokens[index + 3]?.type === types.COMMA
+		&& tokens[index + 4]?.type === types.Y
 	,
 	isIndirect: () =>
-		tokens[index]?.type === types.OPEN_BRACKET &&
-		tokens[index + 1]?.type === types.HEX_VALUE &&
-		tokens[index + 2]?.type === types.NUMBER &&
-		tokens[index + 3]?.type === types.CLOSE_BRACKET &&
-		tokens[index + 4]?.type !== types.COMMA
+		tokens[index]?.type === types.OPEN_BRACKET
+		&& tokens[index + 1]?.type === types.HEX_VALUE
+		&& tokens[index + 2]?.type === types.NUMBER
+		&& tokens[index + 3]?.type === types.CLOSE_BRACKET
+		&& tokens[index + 4]?.type !== types.COMMA
 	,
 	isIndirectX: () =>
-		tokens[index]?.type === types.OPEN_BRACKET &&
-		tokens[index + 1]?.type === types.HEX_VALUE &&
-		tokens[index + 2]?.type === types.NUMBER &&
-		tokens[index + 3]?.type === types.COMMA &&
-		tokens[index + 4]?.type === types.X &&
-		tokens[index + 5]?.type === types.CLOSE_BRACKET
+		tokens[index]?.type === types.OPEN_BRACKET
+		&& tokens[index + 1]?.type === types.HEX_VALUE
+		&& tokens[index + 2]?.type === types.NUMBER
+		&& tokens[index + 3]?.type === types.COMMA
+		&& tokens[index + 4]?.type === types.X
+		&& tokens[index + 5]?.type === types.CLOSE_BRACKET
 	,
 	isIndirectY: () =>
-		tokens[index]?.type === types.OPEN_BRACKET &&
-		tokens[index + 1]?.type === types.HEX_VALUE &&
-		tokens[index + 2]?.type === types.NUMBER &&
-		tokens[index + 3]?.type === types.CLOSE_BRACKET &&
-		tokens[index + 4]?.type === types.COMMA &&
-		tokens[index + 5]?.type === types.Y
+		tokens[index]?.type === types.OPEN_BRACKET
+		&& tokens[index + 1]?.type === types.HEX_VALUE
+		&& tokens[index + 2]?.type === types.NUMBER
+		&& tokens[index + 3]?.type === types.CLOSE_BRACKET
+		&& tokens[index + 4]?.type === types.COMMA
+		&& tokens[index + 5]?.type === types.Y
 	,
 	isHexValue: () =>
-		tokens[index]?.type === types.VALUE &&
-		tokens[index + 1]?.type === types.HEX_VALUE &&
-		tokens[index + 2]?.type === types.NUMBER
+		tokens[index]?.type === types.VALUE
+		&& tokens[index + 1]?.type === types.HEX_VALUE
+		&& tokens[index + 2]?.type === types.NUMBER
 	,
 	isBinValue: () =>
-		tokens[index]?.type === types.VALUE &&
-		tokens[index + 1]?.type === types.BIN_VALUE &&
-		tokens[index + 2]?.type === types.NUMBER
+		tokens[index]?.type === types.VALUE
+		&& tokens[index + 1]?.type === types.BIN_VALUE
+		&& tokens[index + 2]?.type === types.NUMBER
 	,
 	isValue: () =>
-		tokens[index]?.type === types.VALUE &&
-		tokens[index + 1]?.type === types.NUMBER
+		tokens[index]?.type === types.VALUE
+		&& tokens[index + 1]?.type === types.NUMBER
 	,
 	isHexAddress: () =>
-		tokens[index]?.type === types.HEX_VALUE &&
-		tokens[index + 1]?.type === types.NUMBER &&
-		tokens[index + 2]?.type !== types.COMMA
+		tokens[index]?.type === types.HEX_VALUE
+		&& tokens[index + 1]?.type === types.NUMBER
+		&& tokens[index + 2]?.type !== types.COMMA
 	,
 	isHexAddressX: () =>
-		tokens[index]?.type === types.HEX_VALUE &&
-		tokens[index + 1]?.type === types.NUMBER &&
-		tokens[index + 2]?.type === types.COMMA &&
-		tokens[index + 3]?.type === types.X
+		tokens[index]?.type === types.HEX_VALUE
+		&& tokens[index + 1]?.type === types.NUMBER
+		&& tokens[index + 2]?.type === types.COMMA
+		&& tokens[index + 3]?.type === types.X
 	,
 	isHexAddressY: () =>
-		tokens[index]?.type === types.HEX_VALUE &&
-		tokens[index + 1]?.type === types.NUMBER &&
-		tokens[index + 2]?.type === types.COMMA &&
-		tokens[index + 3]?.type === types.Y
+		tokens[index]?.type === types.HEX_VALUE
+		&& tokens[index + 1]?.type === types.NUMBER
+		&& tokens[index + 2]?.type === types.COMMA
+		&& tokens[index + 3]?.type === types.Y
 	,
 	isAbsoluteLabel: () =>
-		tokens[index]?.type === types.LABEL_VALUE &&
-		tokens[index + 1]?.type !== types.COMMA
+		tokens[index]?.type === types.LABEL_VALUE
+		&& tokens[index + 1]?.type !== types.COMMA
 	,
 	isAbsoluteLabelX: () =>
-		tokens[index]?.type === types.LABEL_VALUE &&
-		tokens[index + 1]?.type === types.COMMA &&
-		tokens[index + 2]?.type === types.X
+		tokens[index]?.type === types.LABEL_VALUE
+		&& tokens[index + 1]?.type === types.COMMA
+		&& tokens[index + 2]?.type === types.X
 	,
 	isAbsoluteLabelY: () =>
-		tokens[index]?.type === types.LABEL_VALUE &&
-		tokens[index + 1]?.type === types.COMMA &&
-		tokens[index + 2]?.type === types.Y
+		tokens[index]?.type === types.LABEL_VALUE
+		&& tokens[index + 1]?.type === types.COMMA
+		&& tokens[index + 2]?.type === types.Y
 })
 
 export const recognizeInstructionsValueConst = (tokens, index) => ({
 	isIndirect: () =>
-		tokens[index]?.type === types.OPEN_BRACKET &&
-		tokens[index + 1]?.type === types.CONST_VALUE &&
-		tokens[index + 2]?.type === types.CLOSE_BRACKET &&
-		tokens[index + 3]?.type !== types.COMMA
+		tokens[index]?.type === types.OPEN_BRACKET
+		&& tokens[index + 1]?.type === types.CONST_VALUE
+		&& tokens[index + 2]?.type === types.CLOSE_BRACKET
+		&& tokens[index + 3]?.type !== types.COMMA
 	,
 	isIndirectX: () =>
-		tokens[index]?.type === types.OPEN_BRACKET &&
-		tokens[index + 1]?.type === types.CONST_VALUE &&
-		tokens[index + 2]?.type === types.COMMA &&
-		tokens[index + 3]?.type === types.X &&
-		tokens[index + 4]?.type === types.CLOSE_BRACKET
+		tokens[index]?.type === types.OPEN_BRACKET
+		&& tokens[index + 1]?.type === types.CONST_VALUE
+		&& tokens[index + 2]?.type === types.COMMA
+		&& tokens[index + 3]?.type === types.X
+		&& tokens[index + 4]?.type === types.CLOSE_BRACKET
 	,
 	isIndirectY: () =>
-		tokens[index]?.type === types.OPEN_BRACKET &&
-		tokens[index + 1]?.type === types.CONST_VALUE &&
-		tokens[index + 2]?.type === types.CLOSE_BRACKET &&
-		tokens[index + 3]?.type === types.COMMA &&
-		tokens[index + 4]?.type === types.Y
+		tokens[index]?.type === types.OPEN_BRACKET
+		&& tokens[index + 1]?.type === types.CONST_VALUE
+		&& tokens[index + 2]?.type === types.CLOSE_BRACKET
+		&& tokens[index + 3]?.type === types.COMMA
+		&& tokens[index + 4]?.type === types.Y
 	,
 	isAbsolute: () =>
-		tokens[index]?.type === types.CONST_VALUE &&
-		tokens[index + 1]?.type !== types.COMMA
+		tokens[index]?.type === types.CONST_VALUE
+		&& tokens[index + 1]?.type !== types.COMMA
 	,
 	isAbsoluteX: () =>
-		tokens[index]?.type === types.CONST_VALUE &&
-		tokens[index + 1]?.type === types.COMMA &&
-		tokens[index + 2]?.type === types.X
+		tokens[index]?.type === types.CONST_VALUE
+		&& tokens[index + 1]?.type === types.COMMA
+		&& tokens[index + 2]?.type === types.X
 	,
 	isAbsoluteY: () =>
-		tokens[index]?.type === types.CONST_VALUE &&
-		tokens[index + 1]?.type === types.COMMA &&
-		tokens[index + 2]?.type === types.Y
+		tokens[index]?.type === types.CONST_VALUE
+		&& tokens[index + 1]?.type === types.COMMA
+		&& tokens[index + 2]?.type === types.Y
 })
